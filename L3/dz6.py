5adbb025f0061a0beba9667ac7f4015e79c1fc10
Danko_05 <danylolys13lys@gmail.com>
2024-03-17 22:22:59 +0200

    improved code

--- L3/dz6.py
+++ L3/dz6.py
@@ -3,12 +3,11 @@
 # [] => []
 # [12, 3, 4, 10, 8] => [8, 12, 3, 4, 10]
 
-my_list = []
+my_list = [12, 3, 4, 10, 8]
 len_my_list = len(my_list)
 if len_my_list == 0:
     print("Ваш список порожній:", my_list)
 else:
-    last_index = my_list[-1]
+    last_index = my_list.pop()
     my_list.insert(0, last_index)
-    my_list.pop()
     print(my_list)
