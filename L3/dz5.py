cac4d2ef6a95f90f295e413f693ce99a7d842d59
Danko_05 <danylolys13lys@gmail.com>
2024-03-17 22:20:51 +0200

    Fix code

--- L3/dz5.py
+++ L3/dz5.py
@@ -4,14 +4,29 @@
 # [1] => [[1], []]
 # [] => [[], []]
 
-my_list = [1, 2, 3]
+my_list = [1, 2, 3, 4, 5, 62, 3, 4, 5, 100, 120]
 list_len = len(my_list)
 mid_list = list_len // 2
-if mid_list % 2 == 1 and list_len > 3:
-    first_list = my_list[0: mid_list]
-    second_list = my_list[mid_list: 9]
-else:
+if mid_list % 2 == 1 and list_len % 2 == 1:
     first_list = my_list[0: 1 + mid_list]
-    second_list = my_list[mid_list + 1: 9]
-finally_list = [first_list] + [second_list]
-print(finally_list)
+    second_list = my_list[mid_list + 1:]
+    finally_list = [first_list] + [second_list]
+    print(finally_list)
+
+if mid_list % 2 == 1 and list_len % 2 == 0:
+    first_list = my_list[0: mid_list]
+    second_list = my_list[mid_list:]
+    finally_list = [first_list] + [second_list]
+    print(finally_list)
+
+if mid_list % 2 == 0 and list_len % 2 == 0:
+    first_list = my_list[0: mid_list]
+    second_list = my_list[mid_list:]
+    finally_list = [first_list] + [second_list]
+    print(finally_list)
+
+if mid_list % 2 == 0 and list_len % 2 == 1:
+    first_list = my_list[0: mid_list + 1]
+    second_list = my_list[mid_list + 1:]
+    finally_list = [first_list] + [second_list]
+    print(finally_list)
