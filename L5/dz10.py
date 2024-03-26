1af1968413b29d3a2732cebfd2498cfde6245f73
Danko_05 <danylolys13lys@gmail.com>
2024-03-26 20:15:43 +0200

    Перевірка Імені змінної

---
+++ L5/dz10.py
@@ -0,0 +1,24 @@
+import keyword
+import string
+
+name = input('Введіть назву: ')
+
+valid = True
+
+if name[0].isdigit():
+    valid = False
+
+if name.isdigit():
+    valid = False
+
+if name in keyword.kwlist:
+    valid = False
+
+for char in name:
+    if char.isupper() or char in string.punctuation.replace('_', ''):
+        valid = False
+
+if name.count('__') > 0:
+    valid = False
+
+print(valid)
