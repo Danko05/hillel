9b388e197ab94cd9c9fcf706e9d604f1c8c13c0b
Danko_05 <danylolys13lys@gmail.com>
2024-03-27 23:26:33 +0200

    Конвертер із числа в дату

--- L6/dz15.py
+++ L6/dz15.py
@@ -1,19 +1,23 @@
-x = 950400
-s = 0
-m = 60
-h = 3600
-d = 86400
-days, x = divmod(x, d)
-print(days)
-hours, x = divmod(x, h)
-print(hours)
-minutes, x = divmod(x, m)
-print(minutes)
-seconds = x
-print(seconds)
-if days % 10 == 1:
-    print('день')
-elif 2 <= days % 10 <= 4 and days % 10 == 0:
-    print('днів')
-print(days, str(hours).zfill(2), ":", str(minutes).zfill(2), ":", str(seconds).zfill(2))
+x = int(input('Введіть число:'))
+if 0 <= x <= 8640000:
+    s = 0
+    m = 60
+    h = 3600
+    d = 86400
 
+    days, x = divmod(x, d)
+    hours, x = divmod(x, h)
+    minutes, x = divmod(x, m)
+    seconds = x
+
+    a = ''
+    if 5 <= days <= 20 or days == 11 or days == 0 or 6 <= days % 10 <= 9 :
+        a = 'днів,'
+    elif days % 10 == 1:
+        a = 'день,'
+    elif 2 <= days % 10 <= 4:
+        a = 'дні,'
+    print(days, a, str(hours).zfill(2), ":", str(minutes).zfill(2), ":", str(seconds).zfill(2))
+
+else:
+    print('Введіть число від 0 до 8640000')
