db0f175b5b0870a5a4c8635ddbcd7bf249214c65
Danko_05 <danylolys13lys@gmail.com>
2024-03-27 12:07:47 +0200

    Додав перевірку на пробіл

--- L5/dz10.py
+++ L5/dz10.py
@@ -1,24 +1,26 @@
 import keyword
 import string
+while True:
+    name = input('Введіть назву: ')
 
-name = input('Введіть назву: ')
+    valid = True
 
-valid = True
-
-if name[0].isdigit():
-    valid = False
-
-if name.isdigit():
-    valid = False
-
-if name in keyword.kwlist:
-    valid = False
-
-for char in name:
-    if char.isupper() or char in string.punctuation.replace('_', ''):
+    if name[0].isdigit():
         valid = False
 
-if name.count('__') > 0:
-    valid = False
+    if name.isdigit():
+        valid = False
 
-print(valid)
+    if name in keyword.kwlist:
+        valid = False
+
+    for char in name:
+        if char.isupper() or char in string.punctuation.replace('_', ''):
+            valid = False
+        if char == " ":
+            valid = False
+
+    if name.count('__') > 0:
+        valid = False
+
+    print(valid)
