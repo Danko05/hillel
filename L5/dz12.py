19779844b2359e3db7327fbefbe3c86f5aec437a
Danko_05 <danylolys13lys@gmail.com>
2024-03-27 11:53:19 +0200

    додав обмеження в 140 символів

--- L5/dz12.py
+++ L5/dz12.py
@@ -6,7 +6,7 @@ for i in hashtag:
     if i not in string.punctuation:
         result_string += i
 result = "#" + "".join(result_string).title()
-result1 = result.replace(" ", '')[:6]
+result1 = result.replace(" ", '')[:140]
 print(result1)
 
 
