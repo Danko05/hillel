0612dccbe7290b41f16c2c5ebe8d32f34e7a7b99
Danko_05 <danylolys13lys@gmail.com>
2024-04-23 13:10:29 +0300

    Виняток користувача

--- L13/dz33.py
+++ L13/dz33.py
@@ -18,12 +18,20 @@ class Student(Human):
         return f"{super().__str__()},{self.record_book}"
 
 
+class UserException(Exception):
+    def __init__(self, message= "Максимум 10 людей у групу"):
+        self.message = message
+        super().__init__(self.message)
+
+
 class Group:
     def __init__(self, number):
         self.number = number
         self.group = set()
 
     def add_student(self, student):
+        if len(self.group) >= 10:
+            raise UserException
         self.group.add(student)
         return self.group
 
@@ -48,9 +56,28 @@ class Group:
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st4 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st5 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st6 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st7 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st8 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st9 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st10 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st11 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
+gr.add_student(st3)
+gr.add_student(st4)
+gr.add_student(st5)
+gr.add_student(st6)
+gr.add_student(st7)
+gr.add_student(st8)
+gr.add_student(st9)
+gr.add_student(st10)
+gr.add_student(st11)
 print(gr)
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
 assert gr.find_student('Jobs2') is None, 'Test2'
